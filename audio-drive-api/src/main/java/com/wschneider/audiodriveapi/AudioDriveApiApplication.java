da8b6228fc5b70a81560b43889a3031e087aed4e
Wesley Schneider <wesleyasch@gmail.com>
2022-08-03 12:15:58 -0400

    Initial commit for Java Spring back end

---
+++ audio-drive-api/src/main/java/com/wschneider/audiodriveapi/AudioDriveApiApplication.java
@@ -0,0 +1,13 @@
+package com.wschneider.audiodriveapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioDriveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioDriveApiApplication.class, args);
+	}
+
+}
---
+++ audio-drive-api/src/test/java/com/wschneider/audiodriveapi/AudioDriveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wschneider.audiodriveapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioDriveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
