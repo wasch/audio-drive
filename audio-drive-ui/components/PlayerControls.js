8915a31978385f71f7c5cd3a9632fd8c0f88f060
Wesley Schneider <wesleyasch@gmail.com>
2022-10-03 22:33:03 -0400

    Add styling for playback speed buttons

--- audio-drive-ui/components/PlayerControls.js
+++ audio-drive-ui/components/PlayerControls.js
@@ -1,11 +1,14 @@
-import React, { useEffect } from 'react'
-import AudioPlayer from 'react-h5-audio-player'
+import React, { useEffect, Fragment, useState } from 'react'
+import AudioPlayer, { RHAP_UI } from 'react-h5-audio-player'
 import 'react-h5-audio-player/lib/styles.css'
+
+import { Menu, Popover, Transition } from '@headlessui/react'
 
 import { useDispatch, useSelector } from 'react-redux'
 import { next, previous } from '../redux/slices/queueIndexSlice'
 import { increment, decrement } from '../redux/slices/playbackSpeedSlice'
 import { toggleShouldMaintainPitch } from '../redux/slices/maintainPitchSlice'
+
 
 const PlayerControls = (props) => {
 
@@ -14,6 +17,9 @@ const PlayerControls = (props) => {
   const queueIndex = useSelector((state) => state.queueIndex.value);
   const queue = useSelector((state) => state.queue.value);
   const playbackSpeed = useSelector((state) => state.playbackSpeed.value);
+
+  // State
+  const [maintainPitchIsToggled, setMaintainPitchIsToggled] = useState(false);
 
   useEffect(() => {
     // Enables next and previous media keys (play and pause work by default)
@@ -25,32 +31,21 @@ const PlayerControls = (props) => {
     navigator.mediaSession.setActionHandler('previoustrack', () => dispatch(previous()));
   }, [queue, queueIndex]);
 
-  useEffect(() => {
-    // Setup slow down and speed up buttons
-    const speedUp = document.querySelector('button.speedUp');
-    speedUp.addEventListener("click", () => {
-      dispatch(increment());
-    });
+  const handleSlowdown = () => {
+    dispatch(decrement());
+  }
 
-    const slowDown = document.querySelector('button.slowDown');
-    slowDown.addEventListener("click", () => {
-      dispatch(decrement());
-    });
+  const handleSpeedup = () => {
+    dispatch(increment());
+  }
 
-    // Setup should maintain pitch toggle
-    const maintainPitchToggle = document.querySelector('input.maintainPitchToggle');
-    maintainPitchToggle.addEventListener("click", () => {
-      dispatch(toggleShouldMaintainPitch());
-    });
-  }, []);
+  const handleToggleMaintainPitch = () => {
+    dispatch(toggleShouldMaintainPitch());
+    setMaintainPitchIsToggled(!maintainPitchIsToggled);
+  }
 
   return (
     <div>
-      <button className="slowDown">Slow Down</button>
-      <h5>{playbackSpeed.toFixed(2)}</h5>
-      <button className="speedUp">Speed Up</button><br />
-      <input type="checkbox" id="maintainPitchToggle" name="maintainPitchToggle" className="maintainPitchToggle" />
-      <label htmlFor="maintainPitchToggle">Maintain Pitch</label>
       <AudioPlayer
         id="audioPlayer"
         autoPlay
@@ -61,6 +56,51 @@ const PlayerControls = (props) => {
         onClickPrevious={(e) => dispatch(previous())}
         volume={0.20}
         // other props here
+        customAdditionalControls={
+          [
+            RHAP_UI.LOOP,
+            <Popover as="div" className="flex mt-1">
+              <div>
+                <Popover.Button title="Other controls">
+                  <i className="material-icons">more_vert</i>
+                </Popover.Button>
+              </div>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Popover.Panel className="flex flex-row justify-center items-center absolute border-2 border-zinc-700 rounded-md px-2 my-1 w-52 h-10 -translate-y-3 translate-x-6 bg-zinc-900 shadow-md rounded-sm z-10">
+                  <button className="slowDown" title="Slow down (5%)" onClick={handleSlowdown}>
+                    <img src="https://img.icons8.com/ios-glyphs/30/FFFFFF/rotate-left.png" />
+                    { /* Source: <a target="_blank" href="https://icons8.com/icon/78748/rotate-left">Rotate Left icon by Icons8</a> */}
+                  </button>
+                  <h5 className="mx-2" title="Playback speed">{playbackSpeed.toFixed(2)}</h5>
+                  <button className="speedUp" title="Speed up (5%)" onClick={handleSpeedup}>
+                    <img src="https://img.icons8.com/ios-glyphs/30/FFFFFF/rotate-right.png" />
+                    { /* Source: <a target="_blank" href="https://icons8.com/icon/78746/rotate-right">Rotate Right icon by Icons8</a> */}
+                  </button>
+                  <div className="border border-l-2 border-zinc-700 h-full ml-4"></div>
+                  <div className="flex flex-row">
+                    <input onClick={handleToggleMaintainPitch} type="checkbox" checked={maintainPitchIsToggled} id="maintainPitchToggle" name="maintainPitchToggle" className="ml-4 maintainPitchToggle" />
+                    <label htmlFor="maintainPitchToggle" title="Maintain pitch when changing speed">
+                      <img src="https://img.icons8.com/windows/32/FFFFFF/tuning-fork.png" />
+                      { /* Source: <a target="_blank" href="https://icons8.com/icon/bSsj0HDWJRzb/tuning-fork">Tuning Fork icon by Icons8</a> */}
+                    </label>
+                  </div>
+                </Popover.Panel>
+              </Transition>
+            </Popover>
+          ]
+        }
+        customVolumeControls={[
+          <div className="ml-4"></div>,
+          RHAP_UI.VOLUME
+        ]}
         showSkipControls
         autoPlayAfterSrcChange
       />
