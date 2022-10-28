de82c8410ac73d151809da877f9bbe9c6d85dfc9
Wesley Schneider <wesleyasch@gmail.com>
2022-10-28 00:39:17 -0400

    Adjust playlist styling

--- components/Navbar.js
+++ components/Navbar.js
@@ -43,7 +43,7 @@ const Navbar = () => {
         <div className="bg-slate-600 shadow-lg">
             <nav className="flex items-center justify-center min-w-fit">
                 <div onClick={() => setMobileMenu(false)}>
-                    <Link href="/"><a className="text-3xl pl-5 w-full md:max-w-md">Audio Drive</a></Link>
+                    <Link href="/"><a className="text-3xl pl-5 md:pr-40 w-full md:max-w-md">Audio Drive</a></Link>
                 </div>
                 <button type="button" onClick={() => setMobileMenu(!mobileMenu)} className="md:hidden ml-auto p-2 text-sm rounded-lg hover:backdrop-brightness-110 mobile-menu-button" aria-controls="navbar-default" aria-expanded="false">
                     <svg className="w-8 h-8" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"></path></svg>
--- components/playlist/Playlist.js
+++ components/playlist/Playlist.js
@@ -54,21 +54,21 @@ const Playlist = (props) => {
 
     return (
         <div>
-            <div className="flex flex-col md:flex-row flex-grow items-center mb-6">
+            <div className="relative flex flex-col flex-grow mb-6">
                 <div className="w-full overflow-scroll">
-                    <h3 className="text-4xl py-1">{name}</h3>
+                    <p className="text-4xl py-1">{name}</p>
                 </div>
-                <div className="flex flex-row items-center md:ml-auto mt-6 md:mt-auto">
+                <div className="flex flex-row mt-4 mr-auto">
                     <button className="flex mr-2 p-1 hover:cursor-pointer" title="Play" onClick={handlePlay}>
                         <i className="material-icons text-4xl">play_circle_filled</i>
                     </button>
                     <button className="flex p-1 hover:cursor-pointer" title="Shuffle play" onClick={handleShufflePlay}>
                         <i className="material-icons text-4xl">shuffle</i>
                     </button>
-                    <button className="flex border-2 rounded-sm border-zinc-500 ml-2 md:ml-5 p-1 hover:cursor-pointer" title="Back" onClick={handleBack}>
-                        <i className="material-icons text-3xl">arrow_back</i>
-                    </button>
                 </div>
+                <button className="absolute top-0 right-0 rounded-sm bg-[#2c2c31] z-10 py-1 hover:cursor-pointer" title="Back" onClick={handleBack}>
+                    <i className="material-icons text-4xl">arrow_back</i>
+                </button>
             </div>
             {playlistAudio.map((item, index) => (
                 <div key={index}>
